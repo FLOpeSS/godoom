9ed2a177fd81501d4a7e7c3a6a449fa3bbf9bc80
FLOpeSS <sneto4413@gmail.com>
2024-06-27 23:28:41 -0300

    Giving a little refactoring

--- main.go
+++ main.go
@@ -28,10 +28,11 @@ func startScreen() {
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.White)
-		rl.DrawText("hello world", p.Position_x, p.Position_y, 20, rl.Pink)
+		rl.DrawText("hello Player", p.Position_x, p.Position_y, 20, rl.Pink)
 		rl.DrawFPS(scr.Width-50, 0)
 		rl.EndDrawing()
 		p.Movement(scr)
+
 	}
 
 }
